033a0628b689b70d1b75af914843a7d2dc5f61ea
Radhika Singhal <134649950+radh7@users.noreply.github.com>
2025-03-09 00:33:00 +0530

    Update Password.py

--- backend/Password.py
+++ backend/Password.py
@@ -1,5 +1,5 @@
 class pwd():
-    def __init__(self, MYSQL_PASSWORD = "SSRI9977"):
+    def __init__(self, MYSQL_PASSWORD = "your_password"):
         self.MYSQL_PASSWORD = MYSQL_PASSWORD  # Set this in your system environment
 
 
